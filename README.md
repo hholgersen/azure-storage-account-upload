0e126c5244f0050868a4d45e6781b37a3b43552d
hholgersen <henning.holgersen@storebrand.no>
2023-01-27 12:51:02 +0100

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Upload data to azure blob storage
+
+Hi,
+This is a small script to help upload data to azure blob storage. It requires a few parameters:
+
+- Path to the file you want to upload
+- What you want the uploaded file to be named
+- Container name you want to upload to
+- The name of the azure storage account
+- The SAS Token
+
+Fill in and run.
---
+++ upload.py
@@ -0,0 +1,34 @@
+# first run "pip install azure-storage-blob"
+
+from azure.storage.blob import BlobServiceClient
+
+
+# Add details here:
+file_to_upload = "<my_folder/data.csv>"
+destination_file_name = "<data_2022_01_01.csv>"
+storage_account_name = "<name of storage account>"
+container_name = "<container-name-to-upload-to>"
+sas_token = "<secret-sas-token>"
+
+
+def write_to_azure_blob(sas_token, container_name, blob_name, file_path):
+  """ upload file to azure blob storage """
+
+  blob_service_client = BlobServiceClient.from_connection_string(sas_token)
+  container_client = blob_service_client.get_container_client(container_name)
+  blob_client = container_client.get_blob_client(blob_name)
+
+  with open(file_path, "rb") as data:
+    blob_client.upload_blob(data)
+
+
+def main():
+
+  connection_string = f"BlobEndpoint=https://{storage_account_name}.blob.core.windows.net/;SharedAccessSignature={sas_token}"
+  
+  
+  write_to_azure_blob(connection_string, container_name, destination_file_name, file_to_upload)
+
+if __name__ == '__main__':
+  main()
+
